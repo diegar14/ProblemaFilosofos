e07c4f211f30cac6ccb8a2941f77451f9116a99c
diegar14 <98950435+diegar14@users.noreply.github.com>
2022-03-17 00:52:28 +0100

    Add files via upload

--- ProblemaFilosofos.py
+++ ProblemaFilosofos.py
@@ -1,6 +1,6 @@
 from multiprocessing import Process
 from multiprocessing import Condition, Lock
-from multiprocessing import Array, Manager
+from multiprocessing import Manager
 
 import time
 import random
@@ -9,6 +9,7 @@ NPHIL = 5
 
 K = 100
 
+"""
 class Table:
     def __init__(self, manager):
         self.size = NPHIL
@@ -27,30 +28,35 @@ class Table:
     def wants_think(self,num):
         self.chopsticks[num] = True
         self.chopsticks[(num+1)%NPHIL] = True
-
+"""
     
 class AnticheatTable:
+    
     def __init__(self, manager):
         self.size = NPHIL
         self.chopsticks = manager.list([True]*NPHIL)
         self.anticheat = manager.list([])
         self.mutex = Lock()
         self.cond = Condition(self.mutex)
-        self.cond2 = Condition(self.mutex)
         
     def wants_eat(self,num):
         if num not in self.anticheat:
             self.anticheat.append(num)
-        
+            
+        #wait_for(lambda: self.anticheat[0] == num)
+        while self.anticheat[0] != num:
+            delay()
+            
         self.mutex.acquire()
-        self.cond.wait_for(lambda: self.anticheat[0] == num)
-        self.cond2.wait_for(lambda: self.chopsticks[num] and self.chopsticks[(num+1)%NPHIL])
+        
+        #self.cond.wait_for(lambda: self.chopsticks[num] and self.chopsticks[(num+1)%NPHIL])
+        while not (self.chopsticks[num] and self.chopsticks[(num+1)%NPHIL]):
+            delay(1000)
+            
         print (f"Philosopher {num} eating")
-        print(self.anticheat)
         self.chopsticks[num] = False
         self.chopsticks[(num+1) % NPHIL] = False
         self.anticheat.pop(0)
-        print(self.anticheat)        
         self.mutex.release()
     
     def wants_think(self,num):
@@ -64,14 +70,16 @@ def delay(n=5):
 def philosopher_task(num:int, table: AnticheatTable):
     cnt = 0
     while cnt < K:
-        print (f"Philosopher {num} thinking")
-        table.wants_eat(num)
         print (f"Philosopher {num} wants to eat")
+        table.wants_eat(num)
+        print (f"Philosopher {num} stops eating")
+        
         delay()
         
         table.wants_think(num)
+        print (f"Philosopher {num} thinking")
+
         delay()
-        print (f"Philosopher {num} stops eating")
         cnt += 1
 
 def main():
